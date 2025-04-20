1700141783da509d25fb26f33809baaba8524eab
soreikomori <soreikomori@outlook.com>
2025-04-20 01:37:40 -0500

    is_spammer_kickable: implementation of spamflagged member evaluation

--- App/core/evaluation_core.py
+++ App/core/evaluation_core.py
@@ -27,3 +27,23 @@ def has_bait_role(guild, member):
     baitroleList = gdb.get_bait_roles(guild.id)
     return any(role.id in baitroleList for role in member.roles)
 
+def is_spammer_kickable(guild, member):
+    """
+    Check if a member is spamflagged and is kickable.
+    A member is considered kickable if the guild chooses to kick spamflagged members.
+
+    Parameters
+    ----------
+    guild : discord.Guild
+        The guild object representing the guild.
+    member : discord.Member
+        The member object representing the user.
+
+    Returns
+    -------
+    bool
+        True if the member is kickable, False otherwise.
+    """
+    isSpammer = member.public_flags.spammer
+    guildKicksSpamflagged = gdb.get_value(guild.id, "kick_spamflagged")
+    return isSpammer and guildKicksSpamflagged
