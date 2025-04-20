32f8893a5cbe0980e59f6df6f252db8202673f6d
soreikomori <soreikomori@outlook.com>
2025-04-19 22:39:52 -0500

    Moved event to other file

--- App/dekomori.py
+++ App/dekomori.py
@@ -6,8 +6,8 @@
 import discord
 from discord.ext import commands
 import os
-from App.help_command import InteractionHelpCommand
-from App.scheduler.stall_loop import stall_loop
+from App.commands.help import InteractionHelpCommand
+from App.core.stall_loop_core import stall_loop
 
 intents = discord.Intents.default()
 intents.members = True
@@ -15,13 +15,6 @@ intents.message_content = True
 
 client = commands.Bot(command_prefix='d!', intents=intents, help_command=InteractionHelpCommand())
 
-@client.event
-async def on_ready():
-    print(f"Dekomori Sanae, koko ni suisu!")
-    print(f"Logged in succesfully as {client.user}.")
-    client.loop.create_task(stall_loop(client))
-
-# Automatically load all cogs in App/commands/
 for filename in os.listdir("App/commands"):
     if filename.endswith(".py") and filename != "__init__.py":
         client.load_extension(f"App.commands.{filename[:-3]}")
