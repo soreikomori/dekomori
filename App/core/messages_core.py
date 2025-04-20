b3e5973f9c36c936d4c5f4d98801be6a7c9a438e
soreikomori <soreikomori@outlook.com>
2025-04-20 18:38:31 -0500

    commands: implementation of add and remove role messages

--- App/core/messages_core.py
+++ App/core/messages_core.py
@@ -7,5 +7,17 @@ def nokos_stall(memberMention, memberName, parsedDuration):
 
 class commands:
     baitrole = {
-        "no_args": lambda: "Hey, what exactly do you want me to do? There's `add` and `remove`!"
+        "no_args": lambda: "Hey, what exactly do you want me to do? There's `add` and `remove`!",
+        "add": {
+            "tried_adding_all": lambda: "Hey, you can't add all roles to the bait roles list!",
+            "valid": lambda roles: f"I added {', '.join(roles)} to the bait roles list!",
+            "existing": lambda roles, plural: f"{', '.join(roles)} {plural} already in the bait roles list...",
+            "invalid": lambda roles, plural: f"{', '.join(roles)} {plural} _deathly_ invalid...",
+        },
+        "remove": {
+            "all_roles_invalid": lambda: "None of those roles are valid...",
+            "valid": lambda roles: f"I removed {', '.join(roles)} from the bait roles list!",
+            "not_in_list": lambda roles, plural: f"{', '.join(roles)} {plural} not in the bait roles list...",
+            "invalid": lambda roles, plural: f"{', '.join(roles)} {plural} _deathly_ invalid...",
+        },
     }
