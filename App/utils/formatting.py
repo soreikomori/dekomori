196d95ecac70d07545371ff4fbdae04952ab0ba5
soreikomori <soreikomori@outlook.com>
2025-04-20 17:46:39 -0500

    Initial info

---
+++ App/utils/formatting.py
@@ -0,0 +1,11 @@
+# usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import discord
+from App.dekomori import client
+from App.utils.startup import globalLogger
+from App.utils.constants import VERSION
+
+from App.core import guilds_db_core as gdb
+from App.utils import logger as logger
+
