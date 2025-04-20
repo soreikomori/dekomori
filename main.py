e5c4a0baca84b8a236a3caea4e1bda699a61dc67
soreikomori <soreikomori@outlook.com>
2025-04-19 22:52:34 -0500

    startup: refactorization of logger initialization

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
 from dotenv import load_dotenv
+from App.utils import startup as startup
 from App.dekomori import client
 
 load_dotenv(dotenv_path="env/.env")
 TOKEN = os.getenv("DISCORD_TOKEN")
+startup.initialize_global_logger()
 client.run(TOKEN)
