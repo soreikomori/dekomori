daa52263c5f5438b1305685bd9e6d4419a0ab738
soreikomori <soreikomori@outlook.com>
2025-04-20 02:40:18 -0500

    nokos_stall: update TODO comment for refactorization, added class for commands

--- App/core/messages_core.py
+++ App/core/messages_core.py
@@ -2,4 +2,10 @@
 # -*- coding: utf-8 -*-
 
 def nokos_stall(memberMention, memberName, parsedDuration):
+    #TODO refactor this, no longer used, check baitrole.py/baitrole for appropriate code 
     return f"**Stall Kick:** {memberMention} ({memberName}) joined earlier, but didn't complete onboarding in {parsedDuration}. They have _NOT_ been kicked, okay!?"
+
+class commands:
+    baitrole = {
+        "no_args": lambda: "Hey, what exactly do you want me to do? There's `add` and `remove`!"
+    }
