13ef379807d4a95a34aa2ac412d0177944215b53
soreikomori <soreikomori@outlook.com>
2025-04-20 19:13:17 -0500

    toggle_dm: implementation of DM action toggling functionality

--- App/core/toggler_core.py
+++ App/core/toggler_core.py
@@ -7,3 +7,19 @@ from App.utils.startup import globalLogger
 from App.utils.constants import VERSION
 from App.utils import exceptions as ex
 
+from App.core import guilds_db_core as gdb
+
+def toggle_dm(guild, action):
+    """
+    Toggle the DM action for a specific guild.
+
+    Parameters
+    ----------
+    guild : int
+        The ID of the guild.
+    action : str
+        The action to be performed. Can be either "kick", "ban", or "stall".
+    """
+    action = "kos" if action == "stall" else action
+    key = f"dm_on_{action}"
+    gdb.toggle(guild, key)
