ea6691a842ec7e1f39939888cbf00b26eabcb771
soreikomori <soreikomori@outlook.com>
2025-04-20 19:20:42 -0500

    toggle: implementation of error handling for missing arguments

--- App/commands/toggle.py
+++ App/commands/toggle.py
@@ -13,3 +13,13 @@ from discord.ext import commands
 from App.core import messages_core as msg
 from App.core import toggler_core as tgl
 from App.utils import logger as logger
+
+@client.hybrid_group(brief="Toggle various settings for Dekomori.")
+@app_commands.default_permissions(manage_roles = True)
+@commands.has_permissions(manage_roles=True)
+async def toggle(ctx):
+    """Toggle various settings for Dekomori. You can check their status with d!config."""
+    guildLogger = logger.getLogger(str(ctx.guild.id))
+    await ctx.send(msg.commands.toggle["no_args"]())
+    guildLogger.error(f"{ctx.author.name} didn't specify any arguments for toggle.")
+
