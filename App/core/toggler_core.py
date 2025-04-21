f4d4626335c65d340ba403cdf93eee2a40eabf8a
soreikomori <soreikomori@outlook.com>
2025-04-20 20:23:00 -0500

    toggle_spamflagged: implementation of rejoin checker toggle functionality

--- App/core/toggler_core.py
+++ App/core/toggler_core.py
@@ -6,6 +6,7 @@ from App.dekomori import client
 from App.utils.startup import globalLogger
 from App.utils.constants import VERSION
 from App.utils import exceptions as ex
+from App.utils import checks as checks
 
 from App.core import guilds_db_core as gdb
 
@@ -77,3 +78,17 @@ def toggle_spamflagged(guild):
         The ID of the guild.
     """
     return gdb.toggle(guild, "kick_spamflagged")
+
+@checks.rejoin_checker.has_ping_roles()
+@checks.rejoin_checker.has_max_join_count()
+@checks.rejoin_checker.action_is_ban()
+def toggle_rejoin_checker(guild):
+    """
+    Toggle the rejoin checker for a specific guild.
+
+    Parameters
+    ----------
+    guild : int
+        The ID of the guild.
+    """
+    return gdb.rjc.toggle(guild)
