69bdc007fc4b3637a8bcbc820837e0acfd2553be
soreikomori <soreikomori@outlook.com>
2025-04-20 21:51:07 -0500

    Initial implementation

---
+++ App/core/set_core.py
@@ -0,0 +1,30 @@
+# usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import discord
+from App.dekomori import client
+from App.utils.startup import globalLogger
+from App.utils.constants import VERSION
+from App.utils import exceptions as ex
+from App.utils import checks as checks
+
+from App.core import guilds_db_core as gdb
+
+
+@checks.can_message_channel()
+def set_log_channel(guild: discord.Guild, channel: discord.TextChannel):
+    """Set the log channel for a given guild.
+
+    Parameters
+    ----------
+    guild : discord.Guild
+        The guild where the log channel will be set.
+    channel : discord.TextChannel
+        The channel to be set as the log channel.
+
+    Raises
+    ------
+    PermissionError
+        If the bot does not have permission to send messages in the specified channel.
+    """
+    gdb.set_log_channel(guild.id, channel.id)
